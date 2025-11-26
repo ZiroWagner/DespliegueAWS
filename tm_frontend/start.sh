2d95b56ef1a5cd50915fe745b17ed2de02dbc603
ZiroWagner <cirot1813@gmail.com>
2025-11-26 18:44:32 -0500

    Deploy 1.8

--- tm_frontend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Crear archivo .env.production dinámicamente
-echo "NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL}" > /app/.env.production
-
-echo "Usando NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL}"
-
-# Iniciar Next.js standalone server
-exec node server.js
